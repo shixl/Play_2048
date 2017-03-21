85acaf76dcbce8c1f474a04344a0781726e238b6
LJDY490 <s_xiaole@163.com>
2017-03-21 11:22:44 +0800

    显示数字的view

--- app/src/main/java/com/example/sxl/view/Game2048Item.java
+++ app/src/main/java/com/example/sxl/view/Game2048Item.java
@@ -1,6 +1,8 @@
 package com.example.sxl.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -44,5 +46,74 @@ public class Game2048Item extends View {
         mRect = new Rect();
         mPaint.getTextBounds(mNumberVal,0,mNumberVal.length(),mRect);
         invalidate();
+    }
+
+    public int getNumber(){
+        return mNumber;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        String mBgColor = "";
+        switch (mNumber){
+            case 0:
+                mBgColor = "#CCC0B3";
+                break;
+            case 2:
+                mBgColor = "#EEE4DA";
+                break;
+            case 4:
+                mBgColor = "#EDE0C8";
+                break;
+            case 8:
+                mBgColor = "#F2B179";// #F2B179
+                break;
+            case 16:
+                mBgColor = "#F49563";
+                break;
+            case 32:
+                mBgColor = "#F5794D";
+                break;
+            case 64:
+                mBgColor = "#F55D37";
+                break;
+            case 128:
+                mBgColor = "#EEE863";
+                break;
+            case 256:
+                mBgColor = "#EDB04D";
+                break;
+            case 512:
+                mBgColor = "#ECB04D";
+                break;
+            case 1024:
+                mBgColor = "#EB9437";
+                break;
+            case 2048:
+                mBgColor = "#EA7821";
+                break;
+            default:
+                mBgColor = "#EA7821";
+                break;
+        }
+
+        mPaint.setColor(Color.parseColor(mBgColor));
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(0,0,getWidth(),getHeight(),mPaint);
+        if(mNumber != 0){
+            drawText(canvas);
+        }
+    }
+
+    /**
+     * 绘制文字
+     * @param canvas
+     */
+    private void drawText(Canvas canvas) {
+        mPaint.setColor(Color.BLACK);
+        float x = (getWidth() - mRect.width())/2;
+        float y = getHeight()/2 + mRect.height()/2;
+        canvas.drawText(mNumberVal,x,y,mPaint);
     }
 }
