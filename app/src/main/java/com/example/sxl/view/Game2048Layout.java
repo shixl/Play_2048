5040601a655c2fd53c5308c3cc770b1e04a3208d
LJDY490 <s_xiaole@163.com>
2017-03-22 14:47:25 +0800

    整体的游戏布局

---
+++ app/src/main/java/com/example/sxl/view/Game2048Layout.java
@@ -0,0 +1,30 @@
+package com.example.sxl.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by LJDY490 on 2017/3/22.
+ */
+
+public class Game2048Layout extends ViewGroup {
+
+
+    public Game2048Layout(Context context) {
+        super(context);
+    }
+
+    public Game2048Layout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Game2048Layout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
